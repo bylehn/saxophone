9d42a94316469052c9f4ff8e349f277acc598cbb
Abhishek Sharma <abhishek@pme10D0035.ad.uchicago.edu>
2024-05-09 18:27:16 -0500

    random additions to check angles

--- jaxnets/simulation.py
+++ jaxnets/simulation.py
@@ -720,7 +720,7 @@ def generate_auxetic(run, number_of_nodes_per_side, perturbation, opt_steps):
     ageing_rate=0.1
     success_frac=0.05
     k_fit = 2.0/(dw**2) 
-    system = utils.System(number_of_nodes_per_side, 26+run, 2.0, 0.2, 1e-1)
+    system = utils.System(number_of_nodes_per_side, 26+run, 2.0, 0.2, 0.1)
     system.initialize()
     system.acoustic_parameters(w_c, dw, nr_trials, ageing_rate, success_frac)
     system.auxetic_parameters(perturbation, delta_perturbation, steps, write_every)
@@ -789,6 +789,89 @@ def generate_auxetic(run, number_of_nodes_per_side, perturbation, opt_steps):
              surface_nodes = system.surface_nodes, poisson = poisson, exit_flag = exit_flag)
     return poisson, exit_flag, R_temp, k_temp, system, shift, displacement
 
+
+def generate_nauxetic(run, number_of_nodes_per_side, perturbation, opt_steps):
+
+    """
+    a version of generate_auxetic with the opposite goal
+    """
+    steps = 50
+    write_every = 1
+    delta_perturbation = 0.1
+    nr_trials=500
+    dw=0.2
+    w_c=2.0
+    ageing_rate=0.1
+    success_frac=0.05
+    k_fit = 2.0/(dw**2) 
+    system = utils.System(number_of_nodes_per_side, 26+run, 2.0, 0.2, 0.1)
+    system.initialize()
+    system.acoustic_parameters(w_c, dw, nr_trials, ageing_rate, success_frac)
+    system.auxetic_parameters(perturbation, delta_perturbation, steps, write_every)
+    displacement = system.displacement
+    shift = system.shift
+    R = system.X
+    k_bond = system.spring_constants
+    auxetic_function = simulate_auxetic_wrapper(R, k_bond, system,shift,displacement)
+    grad_auxetic = jit(grad(auxetic_function, argnums=0))
+    grad_auxetic_k = jit(grad(auxetic_function, argnums=1))
+
+    
+    R_temp = R
+    k_temp = k_bond
+    poisson = 10
+    exit_flag=0
+    """
+    0: max steps reached
+    1: gradients exceeded
+    2: max k_temp exceeded
+    
+    """
+    prev_gradient_max_k = 0
+    prev_gradient_max_R = 0
+
+    for i in range(opt_steps):
+
+        #evaluate gradients for bond stiffness and positions
+        gradients_R = - grad_auxetic(R_temp, k_temp)
+        gradients_k = - grad_auxetic_k(R_temp, k_temp)
+
+        #evaluate maximum gradients
+        gradient_max_k = np.max(np.abs(gradients_k))
+        gradient_max_R = np.max(np.abs(gradients_R))
+
+        #calculate difference in maximum gradients
+        diff_gradient_max_k = gradient_max_k - prev_gradient_max_k
+        diff_gradient_max_R = gradient_max_R - prev_gradient_max_R
+    
+        #check if difference in gradients exceed a threshold
+        if np.maximum(diff_gradient_max_k, diff_gradient_max_R) > 10.:
+            print(i, diff_gradient_max_k, diff_gradient_max_R)
+            exit_flag = 1
+            break
+        
+        prev_gradient_max_k = gradient_max_k
+        prev_gradient_max_R = gradient_max_R
+
+        #check if k_temp has exceeded a threshold
+        if np.max(k_temp)>10:
+            exit_flag = 2
+            break
+
+        #update k and R
+        k_temp = utils.update_kbonds(gradients_k, k_temp, learning_rate = 0.02)
+        R_temp = utils.update_R(gradients_R, R_temp,0.01)
+
+        #evaluate new fitness for reporting
+        poisson, log, R_init, R_final = simulate_auxetic(R_temp,
+                                                                k_temp,
+                                                                system,
+                                                                shift,
+                                                                displacement)
+        print(i, gradient_max_k, gradient_max_R,  poisson)
+    onp.savez(str(run), R_temp = R_temp, k_temp = k_temp, perturbation = perturbation, connectivity = system.E,
+             surface_nodes = system.surface_nodes, poisson = poisson, exit_flag = exit_flag)
+    return poisson, exit_flag, R_temp, k_temp, system, shift, displacement
 
 
 def generate_auxetic_acoustic_adaptive(run, number_of_nodes_per_side, perturbation, w_c, dw, poisson_target, opt_steps):
