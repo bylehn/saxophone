added the sigmoid which seems to fix the problem, the systems navigate the gradient jumps now for most conditions instead of getting stuck. there is occasional oscillatory behavior in this optimization problem, might need overhaul. Added k_evolution to evolution_log and added a visualization function to go with it. Still need to test the energy penalty with other functions to see if they handle it well. Also implemented min_k

saxophone/.ipynb_checkpoints/simulation-checkpoint.py
@@ -839,7 +839,9 @@ def generate_auxetic(run, number_of_nodes_per_side, k_angle, perturbation, opt_s
     """
     prev_gradient_max = 0
     R_evolution = np.zeros((opt_steps, system.N, 2))
-    R_evolution = R_evolution.at[0].set(R)
+    R_evolution = R_evolution.at[0].set(R_temp)
+    k_evolution = np.zeros((opt_steps, k_temp.shape[0], 1))
+    k_evolution = k_evolution.at[0].set(k_temp)
     for i in range(opt_steps):
 
         #evaluate gradients for bond stiffness and positions
@@ -876,13 +878,18 @@ def generate_auxetic(run, number_of_nodes_per_side, k_angle, perturbation, opt_s
                                                                 system,
                                                                 shift,
                                                                 displacement)
+        
         print(i, gradient_max,  poisson, energies.penalty_energy(R_init, system) )
+
+        #set evolution bits for the network
         R_evolution = R_evolution.at[i+1].set(R_init)
+        k_evolution = k_evolution.at[i+1].set(k_temp)
+        
     onp.savez(str(run), R_temp = R_temp, k_temp = k_temp, perturbation = perturbation, connectivity = system.E,
              k_angle = k_angle, surface_nodes = system.surface_nodes, poisson = poisson, exit_flag = exit_flag)
 
-    R_d = {'position' : R_evolution}
-    return poisson, exit_flag, R_temp, k_temp, system, shift, displacement, R_d
+    evolution_log = {'position' : R_evolution, 'bond_strengths' : k_evolution}
+    return poisson, exit_flag, R_temp, k_temp, system, shift, displacement, evolution_log
 
 
 #@profile

saxophone/energies.py
@@ -84,8 +84,14 @@ def crossing_penalty_function (system, angles):
     """
     # old version return system.crossing_penalty_strength / (1 + np.exp( system.crossing_penalty_steepness*( angles - system.crossing_penalty_threshold ) ) )
     da = angles / system.crossing_penalty_threshold
-    fn = lambda dr: system.crossing_penalty_strength / 2 * (util.f32(1.0) - da) ** 2
-    return np.where(da < 1.0, fn(da), util.f32(0.0))  / (1 + np.exp( 50.0*( da ) ) )
+    fn = lambda da: system.crossing_penalty_strength / 2 * (util.f32(1.0) - da) ** 2 
+    fn2 = lambda da: 1.0 / (1.0+ np.exp( 50.0*( da - 1.0 ) ) )
+
+    soft_function = np.where(da < 1.0, fn(da), util.f32(0.0)) + 1e-3
+    #np.where(da < 1.0, fn(da), util.f32(0.0)) 
+    sigmoid = fn2(da)
+    
+    return soft_function * sigmoid 
     
 # Assume angle_triplets is an array of shape (num_angles, 3)
 # Each row in angle_triplets represents a set of indices (i, j, k)
@@ -99,9 +105,12 @@ def crossing_penalty_function (system, angles):
 #total_angle_energy = np.sum(vectorized_angle_energy(displacement_fn, k, theta_0, angle_triplets_data, current_positions))
 
 def penalty_energy(R, system, **kwargs):
-        displacement = system.displacement
-        crossing_penalty = np.sum(bond_crossing_penalty(system, system.angle_triplets, displacement, R))
-        # Bond energy (assuming that simple_spring_bond is JAX-compatible)
-        node_energy = energy.soft_sphere_pair(displacement, sigma = system.soft_sphere_sigma, epsilon= system.soft_sphere_epsilon)(R, **kwargs)
+    """
+    provides per node penalty energy
+    """
+    displacement = system.displacement
+    crossing_penalty = np.sum(bond_crossing_penalty(system, system.angle_triplets, displacement, R))
+    # Bond energy (assuming that simple_spring_bond is JAX-compatible)
+    node_energy = energy.soft_sphere_pair(displacement, sigma = system.soft_sphere_sigma, epsilon= system.soft_sphere_epsilon)(R, **kwargs)
 
-        return (crossing_penalty + node_energy)/system.N
+    return (crossing_penalty + node_energy)/system.N

saxophone/utils.py
@@ -30,7 +30,7 @@ class System:
         self.crossing_penalty_strength = 2.0 # epsilon of soft angle strength
         self.crossing_penalty_threshold = 0.3 #radians
 
-        self.penalty_scale = 1e-8 #per node penalty energy that scales to 1 unit in objective functions
+        self.penalty_scale = 1e-5 #per node penalty energy that scales to 1 unit in objective functions
        
         
         # Initialize attributes
@@ -285,7 +285,7 @@ def poisson_ratio(initial_horizontal, initial_vertical, final_horizontal, final_
     return -delta_vertical / delta_horizontal
 
 @jit
-def update_kbonds(gradients, k_bond, learning_rate = 0.1, min_k = 0.0):
+def update_kbonds(gradients, k_bond, learning_rate = 0.1, min_k = 0.05):
     """
     Updates spring constants based on gradients.
 
@@ -297,8 +297,8 @@ def update_kbonds(gradients, k_bond, learning_rate = 0.1, min_k = 0.0):
     """
     gradients_perpendicular = gradients - np.mean(gradients)
     gradients_normalized = gradients_perpendicular / np.max(gradients_perpendicular)
-    k_bond_new = min_k +  (1 - min_k) * k_bond * (1 - learning_rate * gradients_normalized)
-#add min k
+    k_bond_new = min_k +  (k_bond - min_k) * (1 - learning_rate * gradients_normalized)
+
     return k_bond_new
 
 @jit